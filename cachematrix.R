9b3e75480d82a254586dc7f046e9c31b6fbadd19
BoshiW <31558930+boshi024@users.noreply.github.com>
2021-03-22 19:02:30 -0400

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## The R script has two function. The first function creates a matrix object and cache its inverse. 
+## The second function examines whether a cached inverse is available. If it's available, it returns the inverse.
+## If it's not available, it calculate the inverse.
+
+## First function
+
+makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+## Second function
+
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
